b5225bf1800d58e8a982ea4bfce96763d269da78
Christopher Maier <christopher.maier@gmail.com>
2010-02-20 21:32:29 -0500

    Added Option flags

--- maven-completion.bash
+++ maven-completion.bash
@@ -44,12 +44,82 @@ __maven_site_phases="
 	site
 	post-site
 	site-deploy
-" 
+"
+
+# Maven's GNU-style option flags
+_maven_double_dash_options="
+    --also-make
+    --also-make-dependents
+    --batch-mode
+    --strict-checksums
+    --lax-checksums
+    --check-plugin-updates
+    --define
+    --errors
+    --encrypt-master-password
+    --encrypt-password
+    --file
+    --fail-at-end
+    --fail-fast
+    --fail-never
+    --global-settings
+    --help
+    --non-recursive
+    --no-plugin-registry
+    --no-plugin-updates
+    --offline
+    --activate-profiles
+    --projects
+    --quiet
+    --reactor
+    --resume-from
+    --settings
+    --update-snapshots
+    --update-plugins
+    --show-version
+    --version
+    --debug
+"
+
+# Maven's simple option flags
+__maven_single_dash_options="
+	-am
+	-amd
+	-B
+	-C
+	-c
+	-cpu
+	-D
+	-e
+	-emp
+	-ep
+	-f
+	-fae
+	-ff
+	-fn
+	-gs
+	-h
+	-N
+	-npr
+	-npu
+	-o
+	-P
+	-pl
+	-q
+	-r
+	-rf
+	-s
+	-U
+	-up
+	-V
+	-v
+	-X
+"
 
 _maven()
 {
     local cur="${COMP_WORDS[COMP_CWORD]}"
-	COMPREPLY=( $(compgen -W "${__maven_clean_phases} ${__maven_default_phases} ${__maven_site_phases}" -- ${cur}) )
+	COMPREPLY=( $(compgen -W "${__maven_clean_phases} ${__maven_default_phases} ${__maven_site_phases} ${__maven_single_dash_options} ${__maven_double_dash_options}" -- ${cur}) )
     return 0
 }
 complete -F _maven mvn
